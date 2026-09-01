1904156b9c5be3db1288e95be8e9bce24e71f4a1
Om Jee Maurya <omjeemaurya1@gmail.com>
2026-09-01 15:50:36 +0530

    add example code for setTimeout usage

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,19 @@
+// setTimeout(function,delay);
+// // functioin -> code you want to execute
+// // delay-> time in milisecond
+// // 1000  milisecond=1 Second
+// // sample example
+
+// console.log("one");
+// console.log("two");
+// console.log("three");
+// setTimeout(function(){
+//     console.log("hello after 5 second");
+// },5000);
+// console.log("four");
+// console.log("five");
+// console.log("six");
+// console.log("seven");
+// setTimeout(()=>{  //anomous function
+//     console.log("hello after 3 second");
+// },3000);
