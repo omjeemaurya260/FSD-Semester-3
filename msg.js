69799ccda06643851f3e604c9ee6826e32ddea1c
Om Jee Maurya <omjeemaurya1@gmail.com>
2026-09-01 16:29:19 +0530

    add nested

--- msg.js
+++ msg.js
@@ -4,11 +4,35 @@
 // msg()
 // setTimeout(msg, 2000)
 
-console.log("one");
-console.log("two");
-setTimeout(()=>{
-    console.log("hello after 5 second");
-},5000);
-console.log("three");
-console.log("four");
-console.log("five");
+// console.log("one");
+// console.log("two");
+// setTimeout(()=>{
+//     console.log("hello after 5 second");
+// },5000);
+// console.log("three");
+// console.log("four");
+// console.log("five");
+
+
+// function getData(dataId){
+//     setTimeout(()=>{console.log("data",dataId)},
+//     3000);
+// }
+// getData(1);
+// getData(2);
+// getData(3);
+
+
+function getData(dataId,getNextData){
+    setTimeout(()=>{
+        console.log("data",dataId);
+        if(getNextData){
+            getNextData();
+        }
+    },3000);
+}
+getData(1,()=>{
+    getData(2,()=>{
+        getData(3);
+    });
+});
