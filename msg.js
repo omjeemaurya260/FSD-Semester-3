7f2db501f87874558a5836c37b07efa79edb9c61
Om Jee Maurya <omjeemaurya1@gmail.com>
2026-09-01 16:12:20 +0530

    Settimer function

--- msg.js
+++ msg.js
@@ -1,3 +1,14 @@
-function msg() {
-    console.log("My name is om jee maurya");
-}
+// function msg() {
+//     console.log("My name is om jee maurya");
+// }
+// msg()
+// setTimeout(msg, 2000)
+
+console.log("one");
+console.log("two");
+setTimeout(()=>{
+    console.log("hello after 5 second");
+},5000);
+console.log("three");
+console.log("four");
+console.log("five");
