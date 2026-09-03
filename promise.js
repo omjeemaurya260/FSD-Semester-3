8d0d6649011988f226b2b4bcfdffc2c582f187b1
Om Jee Maurya <omjeemaurya1@gmail.com>
2026-09-03 11:21:12 +0530

    asynchronous operation

--- promise.js
+++ promise.js
@@ -1,18 +1,37 @@
-// //asynchronous programming
-// //-promise work
-// let promise = new Promise((resolve, reject) => {
-//     console.log("I am a new promise");
-//    // resolve("Promise resolved");
-//     reject("Promise rejected");
-// });
+
+// // promises is object which is cratewd automatically this is used to handle asynchronous operations
+
+// // let promise = new Promise((resolve, reject) => {
+// //     console.log("I am a new promise");
+// //    // resolve("Promise resolved");
+// //     reject("Promise rejected");
+// // });
 
 
 // function getData(dataId) {
 //     return new Promise((resolve, reject) => {
 //         setTimeout(() => {
 //             console.log("data", dataId);
-//             resolve("sucessfully resolved");
+//          //   resolve("sucessfully resolved");
+//                reject("Promise rejected");
 //         }, 8000);
 //     });
 // }
 // let r= getData(10);
+
+
+
+const gerPromise=()=>{
+    return new Promise((resolve, reject) => {
+        console.log("I am a new promise");
+            // resolve("Promise resolved");
+            reject("Promise rejected")
+    });
+};
+let promise=gerPromise();
+    promise.then(() => {
+        console.log("Promise resolved");
+    });
+promise.catch(() => {
+        console.log("Promise rejected");
+    });
