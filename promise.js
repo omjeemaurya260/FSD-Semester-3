db821b8fbc6a8757ccb279568e0a736d958f8e0a
Om Jee Maurya <omjeemaurya1@gmail.com>
2026-09-08 16:10:23 +0530

    adding of promise chain function

--- promise.js
+++ promise.js
@@ -21,17 +21,42 @@
 
 
 
-const gerPromise=()=>{
-    return new Promise((resolve, reject) => {
-        console.log("I am a new promise");
-            // resolve("Promise resolved");
-            reject("Promise rejected")
-    });
-};
-let promise=gerPromise();
-    promise.then(() => {
-        console.log("Promise resolved");
-    });
-promise.catch(() => {
-        console.log("Promise rejected");
-    });
+// const gerPromise=()=>{
+//     return new Promise((resolve, reject) => {
+//         console.log("I am a new promise");
+//             // resolve("Promise resolved");
+//             reject("Promise rejected")
+//     });
+// };
+// let promise=gerPromise();
+//     promise.then(() => {
+//         console.log("Promise resolved");
+//     });
+// promise.catch(() => {
+//         console.log("Promise rejected");
+//     });
+
+
+
+// //promise chain
+// function getData(dataId) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("data", dataId);
+//             resolve("sucessfully resolved");
+//            // reject("Promise rejected");
+//         }, 8000);
+//     });
+// }
+// getData(1)
+// .then((res) => {
+//     return getData(2);})
+// .then((res) => {
+//     return getData(3);})
+// .then((res) => {
+//     console.log(res);
+// });
+
+
+
+//async function 
