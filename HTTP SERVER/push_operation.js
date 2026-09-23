24e0fbcb46784a60a0b942677b644339d8d8e55f
Om Jee Maurya <omjeemaurya1@gmail.com>
2026-09-23 12:44:58 +0530

    push operation

---
+++ HTTP SERVER/push_operation.js
@@ -0,0 +1,23 @@
+const express =require('express');
+const app = express();
+
+app.use(express.json());
+
+let student = [
+    {id:1, name:"om", branch:"cse"},
+    {id:2, name:"ram", branch:"it"},
+];
+
+app.get('/',(req,res)=>{
+    res.send('server is running');
+});
+
+app.post('/students',(req,res)=>{
+    const newStudent = req.body;
+    students.push(newStudent);
+    res.status(201).json({message:'Student added successfully',student:newStudent});
+});
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+});
